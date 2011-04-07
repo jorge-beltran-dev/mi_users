27139bd2c80c7e715a8b24caa6ae1c9f4f6dae2c
JorgeB <jorge@artvisual.net>
2011-04-07 09:27:06 +0200

    adding schema for users table

---
+++ config/schema/users.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `users` (
+  `id` char(36) NOT NULL,
+  `web_id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `group` varchar(15) DEFAULT 'normal',
+  `password` varchar(41) NOT NULL,
+  `email_verified` tinyint(1) NOT NULL DEFAULT '0',
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
